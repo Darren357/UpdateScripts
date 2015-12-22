38f2850b9e0a749ad31f5f04a83fc0ac518ccf15
Darren357 <dlmccaul@mtu.edu>
2015-12-22 01:29:29 -0500

    no message

---
+++ BotEmail.bat
@@ -0,0 +1,9 @@
+::The main operator, started by Thunderbird with the Mailbox Alert add-on
+
+cd C:\Darren\github\UpdateScripts\autodate
+replace.cmd html.txt index.html
+
+
+cd C:\Darren\github\UpdateScripts\pusher 
+::gitpush.cmd
+
